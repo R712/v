992029bb782f13c36c79943a24c0541ef87bf2b4
R712 <reum712@gmail.com>
2021-01-04 07:42:40 +0900

    보관함 및 이달의노래 등 여기저기 수정.
    owl-carousel 추가(vue-carousel 대용)
    보관함 - 아티스트/앨범/플레이리스트(모달팝업테스트가능)/받은노래/구매한노래 추가.

--- js/prototype.js
+++ js/prototype.js
@@ -16,11 +16,11 @@ const vibePlayer = $('#player');
 //
 //  아이템 옵션 레이어
 //
-$('.content').on('click', '.btn_option_now, .btn_option', function(e) {
+$('.content').on('click', '.btn_option_now, .btn_option, .btn_drop_menu', function(e) {
     e.preventDefault();
     let elm = $(this);
-    let elmNav = elm.next('.ly_option');
-    $('.ly_option').not(elmNav).hide();
+    let elmNav = elm.next('.ly_option, .layer_drop_menu');
+    $('.ly_option, .layer_drop_menu').not(elmNav).hide();
     if (e.clientX + 150 > $(window).width()) {
         elmNav.css({
             position: 'fixed',
@@ -37,12 +37,23 @@ $('.content').on('click', '.btn_option_now, .btn_option', function(e) {
     };
     elmNav.toggle();
 });
+$('.layer_drop_menu').on('click', 'a', function() {
+    let selectedItem = $(this).html();
+    $(this).closest('.dropdown_wrap').find('.btn_drop_menu').html(selectedItem);
+});
 headerGNB.on('click', '.profile_area .profile', function(e) {
     e.preventDefault();
     let elm = $(this);
     let elmNav = elm.next('.ly_option');
     $('.ly_option').not(elmNav).hide();
     elmNav.toggle();
+});
+
+// 서브 탭 메뉴
+$('.tab_menu').on('click', '.tab_item', function() {
+    let motherTab = $(this).closest('.tab_menu');
+    motherTab.find('.tab_item').removeClass('selected');
+    $(this).addClass('selected');
 });
 
 //
@@ -100,18 +111,14 @@ headerGNB.on('click', '.btn_search', function() {
 $('.content').on('click', '.VueCarousel-navigation-next', function() {
     let motherSection = $(this).parent('div').find('.scroll_list');
     let sumWidth = 0;
-    let itemCount = motherSection.find('.list_item').length;
-    if (itemCount <= 10) {
-        motherSection.find('.list_item').clone().appendTo(motherSection);
-    }
     motherSection.find('.list_item').each(function() {
         sumWidth += parseInt($(this).width());
     });
-    motherSection.animate({ left: '-=' + parseInt(motherSection.closest('div').width() + 20, 10) + 'px' }, 120);
+    motherSection.animate({ left: '-=' + parseInt(motherSection.closest('div').width() + 20, 10) + 'px' }, 150);
     let motherPosition = motherSection.position();
     let motherX = (parseInt(motherPosition.left) * -1) + motherSection.closest('div').width();
     if (sumWidth <= motherX) {
-        motherSection.animate({ left: '0' }, 200)
+        motherSection.animate({ left: '0' }, 250)
     }
 });
 
@@ -138,7 +145,7 @@ $('.tracklist').on('click', 'input', function() {
 //  전체 선택 체크박스 클릭시
 //
 $('#chk_all, .label_check').click(function() {
-    if($('#chk_all').is(':checked')) {
+    if ($('#chk_all').is(':checked')) {
         $('.tracklist').find(':checkbox').prop('checked', true);
         let selectedTrack = $('.tracklist').find(':checked').length;
         $('.count_track').html(selectedTrack + '곡 선택');
@@ -195,6 +202,10 @@ $('.floating_select').appendTo('.floating_bar');
 $('.btn_like, .btn_add').click(function() {
     $(this).toggleClass('on');
 });
+//  보관함에서 좋아하는 아티스트 취소
+$('.thumb_area').on('click', '.btn_like', function() {
+    $(this).closest('.list_item').addClass('dislike').fadeOut(200);
+});
 
 //
 //  미리보기용 URL 연결
@@ -217,17 +228,35 @@ $('.btn_lyrics, .song').click(function() {
 $('.link_djstation, .link_dj').click(function() {
     location.href = './djstation.html';
 });
+$('.link_monthly').click(function() {
+    location.href = './monthly.html';
+});
 $('.album, .link_album').click(function() {
     location.href = './album.html';
 });
 $('.artist, .link_artist, .link_sub_title').click(function() {
     location.href = './artist.html';
 });
-$('.link_library').on('click', function() {
+$('.link_library, .library_menu .menu_item:first-child').on('click', function() {
     location.href = './archive.html';
 });
 $('.library_menu .menu_item:nth-child(2)').on('click', function() {
     location.href = './archive-song.html';
+});
+$('.library_menu .menu_item:nth-child(3)').on('click', function() {
+    location.href = './archive-artist.html';
+});
+$('.library_menu .menu_item:nth-child(4)').on('click', function() {
+    location.href = './archive-album.html';
+});
+$('.library_menu .menu_item:nth-child(5)').on('click', function() {
+    location.href = './archive-playlist.html';
+});
+$('.library_menu .menu_item:nth-child(6)').on('click', function() {
+    location.href = './archive-received.html';
+});
+$('.library_menu .menu_item:last-child').on('click', function() {
+    location.href = './archive-paid.html';
 });
 $('.membership_menu .item:first-child, .link_membership').on('click', function() {
     location.href = './subscription.html';
@@ -246,23 +275,91 @@ $('.membership_menu .item:nth-child(4)').on('click', function() {
 //  페이지 로더 미리보기용
 //
 let vibeApp;
+
 function showPage() {
     document.querySelector('.loading_vibe').style.display = 'none';
     document.getElementById('container').style.display = 'block';
 }
+
 function loadingCheck() {
     vibeApp = setTimeout(showPage, 1800);
 }
 window.onload = function() {
-    loadingCheck();
-}
-//  미리보기용으로 during이 임의로 들어가있기때문에 클릭하면 닫히게 만듦
+        loadingCheck();
+    }
+    //  미리보기용으로 during이 임의로 들어가있기때문에 클릭하면 닫히게 만듦
 $('.loading_vibe').on('click', function() {
     document.querySelector('.loading_vibe').style.display = 'none';
     document.getElementById('container').style.display = 'block';
 });
 
-// 배너 닫기
+//  배너 닫기
 $('.admin_banner_section, .banner_area').on('click', '.btn_close', function() {
     $('.admin_banner_section').hide();
+});
+
+//  플레이리스트 추가 팝업 모달 레이어 제어
+$('.btn_add_item').click(function() {
+    $('.modal').fadeIn(200);
+});
+$('.ly_btn_area').on('click', 'a[role=button]:not(.point)', function() {
+    $('.modal').fadeOut(200);
+}).on('click', 'a.point', function() {
+    const motherModal = $(this).closest('.modal');
+    let newPlaylist = motherModal.find('input#new_playlist');
+    let bodyPlaylist = `<li class="list_item added__now">
+    <div>
+        <div class="thumb_area">
+            <a class="link">
+                <img src="img/album-dummy.jpg" class="thumb" alt="` + newPlaylist.val() + ` 플레이리스트 커버" />
+            </a>
+            <a role="button" aria-haspopup="dialog" class="btn_play_now"><span class="blind">재생하기</span></a>
+            <div class="more_option only">
+                <div class="dropdown_wrap">
+                    <a role="button" aria-haspopup="menu" aria-expanded="false" class="btn_option_now"><span class="blind">옵션 보기</span></a>
+                    <div role="menu" class="ly_option">
+                        <a role="button" class="ly_item"><span>보관함에서 삭제</span></a>
+                        <a role="button" class="ly_item">바로 다음에 추가</a>
+                        <a role="button" class="ly_item">맨 아래에 추가</a>
+                        <a class="ly_item">MP3 구매</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="info">
+            <a class="title"><span class="text">` + newPlaylist.val() + `</span><span class="blind">페이지로 이동</span></a>
+            <div class="artist"><span>0곡</span>
+            </div>
+        </div>
+    </div>
+</li>`;
+    if (newPlaylist.val()) {
+        $('.sub_list .list_item:first-child').after(bodyPlaylist);
+        motherModal.fadeOut(200);
+    } else if (!$('.form-alert').length) {
+        let errorMsg = `<p class="form-alert">플레이리스트 이름을 확인해주세요.</p>`;
+        newPlaylist.after(errorMsg);
+        $('.form-alert').fadeIn(100);
+    }
+});
+$('.ly_popup').on('keydown', 'input[type=text]', function() {
+    ($(this).val()) ? $('.form-alert').fadeOut(100): false;
+});
+
+//  미리보기용 owlcarousel2
+//  vue-carousel 대용
+$(document).ready(function() {
+    const owl = $('.owl-carousel');
+    let startPosition = false;
+    if (owl.has('.menu_item.on')) {
+        startPosition = owl.find('.menu_item.on').index();
+    }
+    owl.owlCarousel({
+        center: true,
+        margin: 20,
+        startPosition: startPosition,
+        mouseDrag: true,
+        touchDrag: true,
+        autoWidth: true
+    });
 });
