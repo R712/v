774c91d3e63235081c3cbb06a84908877661044b
Ahreum Han <R712@users.noreply.github.com>
2021-01-04 11:15:28 +0900

    모바일 반영중

--- js/prototype.js
+++ js/prototype.js
@@ -283,9 +283,9 @@ function loadingCheck() {
     vibeApp = setTimeout(showPage, 1800);
 }
 window.onload = function() {
-        loadingCheck();
-    }
-    //  미리보기용으로 during이 임의로 들어가있기때문에 클릭하면 닫히게 만듦
+    loadingCheck();
+}
+//  미리보기용으로 during이 임의로 들어가있기때문에 클릭하면 닫히게 만듦
 $('.loading_vibe').on('click', function() {
     document.querySelector('.loading_vibe').style.display = 'none';
     document.getElementById('container').style.display = 'block';
@@ -345,7 +345,7 @@ $('.ly_popup').on('keydown', 'input[type=text]', function() {
 });
 
 //  미리보기용 owlcarousel2
-//  vue-carousel 대용
+//  vue-carousel 대용으로 가져왔는데 역시 완벽히 맘에 들게 커스텀이 잘 안 됨 ㅜㅠ
 $(document).ready(function() {
     let startPosition = false;
     if ($('.owl-carousel').has('.menu_item.on')) {
